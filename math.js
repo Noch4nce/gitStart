764dac8d59c9c1ed06df940df68c73fc2101baf2
Noch4nce <m4rgera123@mail.ru>
2020-07-18 04:40:07 +0500

    Add math.js

---
+++ math.js
@@ -0,0 +1,86 @@
+// let a = 10;
+// let b = 3;
+
+
+// let res = a % b;
+// console.log(a % b);
+
+// const x = 13 % 5;
+// console.log(x);
+
+//2
+// let a = 9;
+// let b = 4;
+
+// let res = a % b;
+// if (res != 0){
+//     console.log('Share = ' + res)
+// } else console.log('Not share' + (a / b))
+
+//3
+// let st = Math.pow(2, 10);
+// console.log(st);
+
+//4
+// console.log(Math.sqrt(245));
+
+//5
+// let array = [4, 2, 5, 19, 13, 0, 10];
+
+// for (i=0; i < array.length; i++){
+//     let pow = Math.pow(array[i], 3);
+//     console.log(Math.sqrt(pow));
+// }
+
+//6
+// let n = Math.sqrt(379);
+// console.log(n);
+// console.log(Math.round(n));
+// console.log(n.toPrecision(1))
+// console.log(n.toFixed(1));
+// console.log(n.toFixed(2))
+
+//7
+// let n = Math.sqrt(587);
+// console.log(n);
+// let obj = {
+//     ceil: Math.ceil(n),
+//     floor: Math.floor(n),
+// }
+// for (key in obj){
+//     console.log(obj[key])
+// }
+
+//8
+// let n = [4, -2, 5, 19, -130, 0, 10];
+// console.log(Math.max.apply(null, n));
+// console.log(Math.min.apply(null, n));
+// console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
+// console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
+
+//9
+// function getRoundRnd(max, min){
+//     return Math.round(Math.random() * (max - min + 1)) + min; 
+// }
+// console.log(getRoundRnd(10, 100))
+
+// console.log(Math.random() * (100 - 10) + 10); 
+
+//10
+// let arr = [];
+// for (i = 0; i < 20; i++){
+//     arr[i] = Math.floor(Math.random() * (125 - 1 + 1) + 10);
+// }
+// console.log(arr);
+
+//11
+// let a = 3;
+// let b = 4;
+// console.log(Math.abs(a - b));
+
+//12
+// let a = 3;
+// let b = 5;
+// let c = Math.abs(a - b);
+// console.log(c);
+
