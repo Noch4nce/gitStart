68986dc9188cb2ab72ecc0531a6ab419282fc4b2
Noch4nce <m4rgera123@mail.ru>
2020-07-19 12:54:11 +0500

    sliderCons

---
+++ sliderCons/sliderObj1.js
@@ -0,0 +1,57 @@
+let slider1 = sliderMain();
+
+// let slider1 = {
+//     leftBtn: null, 
+//     rightBtn: null, 
+
+//     imgMain: null, 
+
+//     imgArray: [],
+//     currentImg: 0,
+
+//     start: function (elId) {
+
+//         let that = this;
+
+//         let selector = '#' + elId;
+
+//         let el = document.querySelector(selector);
+
+//         this.leftBtn = el.querySelector('.slideLeftBtn');
+//         this.rightBtn = el.querySelector('.slideRightBtn');
+//         this.imgMain = el.querySelector('.slideImg');
+
+//         this.leftBtn.addEventListener('click', function(e){
+//             that.leftClick(e);
+//         });
+//         this.rightBtn.addEventListener('click', function(e){
+//             that.rightClick(e);
+//         });
+
+//         this.imgArray.push('https://static.scientificamerican.com/sciam/cache/file/4E0744CD-793A-4EF8-B550B54F7F2C4406_source.jpg');
+//         this.imgArray.push('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-1557340276.jpg');
+//         this.imgArray.push('https://i.ytimg.com/vi/Z4ls0-ZGnU4/maxresdefault.jpg');
+//         this.imgArray.push('https://www.scitecheuropa.eu/wp-content/uploads/2019/09/Nature-climate.jpg');
+
+//         this.imgMain.src = this.imgArray[this.currentImg];
+//         this.leftBtn.disabled = true;
+//     },
+
+//     leftClick: function (e) {
+//         this.currentImg--;
+//         this.imgMain.src = this.imgArray[this.currentImg];
+//         this.rightBtn.disabled = false;
+//         if (this.currentImg == 0) {
+//             this.leftBtn.disabled = true;
+//         }
+//     },
+
+//     rightClick: function (e) {
+//         this.currentImg++;
+//         this.imgMain.src = this.imgArray[this.currentImg];
+//         this.leftBtn.disabled = false;
+//         if (this.currentImg == (this.imgArray.length - 1)) {
+//             this.rightBtn.disabled = true;
+//         }
+//     }
+// };
---
+++ sliderCons/sliderScr.js
@@ -0,0 +1,8 @@
+// let cont1 = document.querySelector('#sliderId1');
+// let cont2 = document.querySelector('#sliderId2');
+
+let slider1 = sliderFact.sliderMain();
+let slider2 = sliderFact.sliderMain();
+
+slider1.start('sliderId1');
+slider2.start('sliderId2');
