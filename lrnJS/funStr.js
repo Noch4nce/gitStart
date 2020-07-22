5137684cbe08dc771e5de2aa148cbfe15f14d684
Noch4nce <m4rgera123@mail.ru>
2020-07-22 17:49:41 +0500

    funJS

---
+++ lrnJS/funStr.js
@@ -0,0 +1,73 @@
+// let l = 'js';
+// console.log(l.toUpperCase());
+
+// let l = "JS";
+// console.log(l.toLowerCase());
+
+// let str = 'I`m learning javascript!';
+// console.log(str.length);
+
+// let str = 'I`m learning javascript!';
+// console.log(str.substr(0, 3));
+// console.log(str.substring(0, 3));
+// console.log(str.slice(0, 3));
+
+// let str = 'I`m learning javascript!';
+// console.log(str.indexOf('learning'));
+
+//6
+// let str = 'some text';
+// let n = 4;
+// if (n < str.length){
+//     let res = str.substr(0, n)
+//     console.log(res + '...')
+// } else (res = str);{
+//     console.log(res);
+// }
+
+// let str = 'I`m-learning-javascript!';
+// console.log(str.replace(/-/g, '!'))
+   
+// let str = 'I`m learning javascript!';
+// console.log(str.split(' '))
+
+// let str = 'I`m learning javascript!';
+// console.log(str.split(''))
+
+// let date = '2025-12-31';
+// let arr = date.split('-');
+// let arr2 = arr.reverse();
+// let res = arr2.join('.');
+// console.log(res);
+
+// let arr = ['я', 'учу', 'javascript', '!'];
+// console.log(arr.join('+'));
+
+// let str = 'string';
+// console.log(str[0].toUpperCase() + str.substr(1));
+
+// let str = 'any word';
+// console.log(str[0].toUpperCase() + str.substr(1, 3) + str[4].toUpperCase() + str.substr(5));
+
+// let str = 'var_test_text';
+// let arr = str.split('_').join('');
+// let res = arr.substr(0, 3) + arr[3].toUpperCase() + arr.substr(4, 3) + arr[7].toUpperCase() + arr.slice(-3);
+// console.log(res)
+
+
+// var str = 'я учу javascript!';
+// //var new_str = str.split(' ');
+
+// // for(var i = 0; i < new_str.length; i++){
+// // new_str[i] = new_str[i].slice(0, 1).toUpperCase() + new_str[i].slice(1);
+// // }
+
+// // console.log(new_str.join(' '));
+
+// console.log(str.slice(1));
+
+// var a = ['a', 'b', 'c'];
+// var b = [1, 2, 3];
+
+// var c = [...a, ...b]
+// console.log(c)
