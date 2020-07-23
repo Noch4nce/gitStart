fc717ade42612b4082bd294dcadc6d1a22758b4a
Noch4nce <m4rgera123@mail.ru>
2020-07-23 11:21:22 +0500

    strFun

---
+++ lrnJS/strFun.js
@@ -0,0 +1,28 @@
+// let str = 'string';
+// console.log(str[0].toUpperCase() + str.substring(1));
+
+// var str = 'hello';
+// str = str.split('');
+// str[0] = str[0].toUpperCase();
+// var result = str.join('');
+// console.log(result);
+
+// let str = '123456';
+// let str1 = str.split('').reverse().join('');
+// console.log(str1);
+
+// let str = 'http://goog.co';
+// if (str.substr(0, 7) == 'http://'){
+//     console.log('ye');
+// }else {
+//     console.log('no');
+// }
+
+// let str = 'index.html';
+// if (str.substr(-5) == '.html'){
+//     console.log('ye');
+// }else {
+//     console.log('no');
+// }
+
+
