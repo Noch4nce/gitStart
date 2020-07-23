41ac3065594f899588e30b73ac21b074cb9382e1
Noch4nce <m4rgera123@mail.ru>
2020-07-23 10:58:05 +0500

    funArr

---
+++ lrnJS/funArr.js
@@ -0,0 +1,59 @@
+// let arr1 = [1, 2, 3];
+// let arr2 = [3, 2, 1];
+// let res = arr1.concat(arr2);
+// console.log(res.join());
+
+// let arr1 = [1, 2, 3];
+// console.log(arr1.reverse());
+
+// let arr1 = [1, 2, 3];
+// arr1.push(4, 5, 6);
+// console.log(arr1);
+
+// let arr1 = [1, 2, 3];
+// arr1.unshift(4, 5, 6);
+// console.log(arr1);
+
+// let arr = ['js', 'css', 'jq'];
+// console.log(arr.shift());
+
+// let arr = ['js', 'css', 'jq'];
+// console.log(arr.pop());
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.slice(0, -2));
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.slice(3, 5));
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.splice(1, 2)
+// console.log(arr)
+
+// let arr = [1, 2, 3, 4, 5];
+// let arr1 = arr.splice(1, 3);
+// console.log(arr1);
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.splice(3, 0, 'a', 'b', 'c');
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5];
+// let arr1 = arr.splice(1, 0, 'a', 'b');
+// let arr2 = arr.splice(6, 0, 'c');
+// arr.push('e');
+// console.log(arr);
+
+// let arr = [3, 4, 1, 2, 7];
+// console.log(arr.sort());
+
+// let obj = {
+//     js:'test', 
+//     jq: 'hello', 
+//     css: 'world'
+// }
+// // for (key in obj){
+// //     console.log(key);
+// // }
+// console.log(Object.keys(obj));
+
