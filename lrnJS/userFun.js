6a95792814d6c4e40f63f83a776aa9acfe46a6ee
Noch4nce <m4rgera123@mail.ru>
2020-07-23 15:04:41 +0500

    userFun

---
+++ lrnJS/userFun.js
@@ -0,0 +1,107 @@
+// function cube(num) {
+//     let res = num * num * num;
+// 	return res;
+// }
+// console.log(cube(3));
+
+// function sum (num) {
+//     let res = num + num;
+//     return res;
+// }
+// console.log(sum(3))
+
+// function exp () {
+//     let res = (5 - 1) / 2;
+//     console.log(res);
+// }
+// exp();
+
+// function exp (num1, num2, num3) {
+//     let res = (num1 - num2) / num3;
+//     console.log(res);
+// }
+// exp(5, 1, 2);
+
+// function forNum (res) {
+//     for (i = 1; i <= 7; i ++){
+//         let a = res[i]
+//         console.log(a);
+//     }
+// }
+// forNum()
+
+//let asd = ['a', 'c', 'b'];
+//let sum = 3;
+//for (i = 1; i <= 7; i ++){
+//let sum = i;
+//console.log(sum)
+//}
+
+// function forNum() {
+//     let a = 7;
+//     for (i = 1; a <= 7; i ++){
+//         console.log(a)
+//     }
+// }
+
+//forNum()
+
+
+// for (i = 1; i <= 7; i ++){
+//     let num = i;
+// }
+// console.log(i);
+
+// function asd (num) {
+//     console.log(num)
+// }
+// asd()
+
+
+// let week = ['poned', 'sreda', 'vtor'];
+// for (i = 0; i < week.length; i++) {
+//     if (i == 0){
+//         console.log(week[i])
+//     } else if (i == 1){
+//         console.log(week[i])
+//     } else if (i == 2){
+//         console.log(week[i])
+//     } else {
+//         console.log('idk')
+//     }
+// }
+
+// let week = ['poned', 'sreda', 'vtor'];
+// console.log(week[0] + '1');
+
+// function dWeek (num) { 
+//     if (num == 1){
+//         return 'poned';
+//     }
+// }
+// console.log(dWeek(1))
+
+// function week (day) {
+//     if (day == 1) {
+//         return "понедельник";
+//     }
+//     else if (day == 2) {
+//         return "вторник";
+//     }
+//     else if (day == 3) {
+//         return "среда";
+//     }
+//     else if (day == 4) {
+//         return "четверг";
+//     }
+//     else if (day == 5) {
+//         return "пятница";
+//     }
+//     else if (day == 6) {
+//         return "суббота";
+//     }
+//     else {
+//         return "воскресенье";
+//     }
+// }
+// console.log(day(2));
